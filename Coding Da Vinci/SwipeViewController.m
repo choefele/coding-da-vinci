5a71fd1aceaba9c482466f4fb447542ae536b770
Claus Höfele <claus@claushoefele.com>
2014-05-14 22:17:51 +0200

    Use core image to swipe image

--- Coding Da Vinci/SwipeViewController.m
+++ Coding Da Vinci/SwipeViewController.m
@@ -8,42 +8,78 @@
 
 #import "SwipeViewController.h"
 
+#import <CoreImage/CoreImage.h>
+
 @interface SwipeViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (nonatomic) CIContext *contextCI;
+@property (nonatomic) UIImage *inputImage;
+@property (nonatomic) UIImage *targetImage;
+@property (nonatomic, getter = isInProgress) BOOL inProgress;
 
 @end
 
 @implementation SwipeViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.contextCI = [CIContext contextWithOptions:nil];
+    self.inputImage = [UIImage imageNamed:@"osterdeich-bremen.jpg"];
+    self.targetImage = [UIImage imageNamed:@"osterdeich-bremen-strandpromenade.jpg"];
+    
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
+    [self.imageView addGestureRecognizer:panGestureRecognizer];
+    self.imageView.userInteractionEnabled = YES;
 }
 
-- (void)didReceiveMemoryWarning
+- (void)handlePanGesture:(UIPanGestureRecognizer *)panGestureRecognizer
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CGPoint point = [panGestureRecognizer locationInView:self.imageView];
+    double progress = point.x / self.imageView.bounds.size.width;
+    
+    if (self.isInProgress == NO) {
+        self.inProgress = YES;
+        [self createFilteredImageForInputImage:self.inputImage targetImage:self.targetImage progress:progress completionBlock:^(UIImage *image) {
+            self.imageView.image = image;
+            self.inProgress = NO;
+        }];
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
++ (CIFilter *)filterWithInputImage:(UIImage *)inputImage targetImage:(UIImage *)targetImage
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    CIImage *inputImageCI = [CIImage imageWithCGImage:inputImage.CGImage];
+    CIImage *targetImageCI = [CIImage imageWithCGImage:targetImage.CGImage];
+    CIFilter *filter = [CIFilter filterWithName:@"CISwipeTransition" keysAndValues:kCIInputImageKey, inputImageCI, kCIInputTargetImageKey, targetImageCI, nil];
+
+    [filter setValue:@0 forKey:@"inputWidth"];
+    CIVector *inputExtent = [CIVector vectorWithCGRect:CGRectMake(0, 0, targetImage.size.width, targetImage.size.height)];
+    [filter setValue:inputExtent forKey:@"inputExtent"];
+    
+    return filter;
 }
-*/
+
+- (void)createFilteredImageForInputImage:(UIImage *)inputImage targetImage:(UIImage *)targetImage progress:(double)progress completionBlock:(void (^)(UIImage *image))completionBlock
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        CIFilter *filter = [self.class filterWithInputImage:inputImage targetImage:targetImage];
+        [filter setValue:@(progress) forKey:@"inputTime"];
+
+        CIImage *outputImage = [filter outputImage];
+        CGImageRef outputImageCG = [self.contextCI createCGImage:outputImage fromRect:outputImage.extent];
+        UIImage *newImage = [UIImage imageWithCGImage:outputImageCG];
+        CGImageRelease(outputImageCG);
+        
+        if (completionBlock) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completionBlock(newImage);
+            });
+        }
+    });
+}
 
 @end
